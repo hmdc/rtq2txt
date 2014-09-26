758e76189165912c0ca0b2db9676654ad777132a
Evan Sarmiento <esarmien@g.harvard.edu>
2014-09-26 15:47:47 -0400

    Adding setup.py, etc

--- scripts/rtq2txt.py
+++ scripts/rtq2txt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import argparse
 from rtq2txt import rtq2txt
 import rt
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='rtq2txt',
+      version='0.1',
+      description='Reads tickets from an Request Tracker (RT) queue and outputs them in .txt',
+      url='https://github.com/hmdc/rtq2txt',
+      author='Evan Sarmiento',
+      author_email='esarmien@g.harvard.edu',
+      license='MIT',
+      packages=['rtq2txt'],
+      requires=['rt'],
+      scripts=['scripts/rtq_mark_spam.sh', 'scripts/rtq2txt.py']
+)
