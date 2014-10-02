cb7c166b7f65d123e5bcbf57f85324a47827dafe
Evan Sarmiento <esarmien@g.harvard.edu>
2014-10-02 13:41:57 -0400

    Using setuptools is better.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-
+from setuptools import setup, find_packages
 setup(name='rtq2txt',
       version='0.1',
       description='Reads tickets from an Request Tracker (RT) queue and outputs them in .txt',
@@ -8,6 +7,6 @@ setup(name='rtq2txt',
       author_email='esarmien@g.harvard.edu',
       license='MIT',
       packages=['rtq2txt'],
-      requires=['rt'],
+      install_requires=['rt'],
       scripts=['scripts/rtq_mark_spam.sh', 'scripts/do_rtq2txt.py']
 )
